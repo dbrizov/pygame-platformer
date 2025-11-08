e55d6cb069c4bbd07d81e1a90507c74490a72894
dbrizov <d.b.rizov@gmail.com>
2025-11-08 13:35:38 +0200

    Optimized Vec2 constants

--- assets/src/engine/math.py
+++ assets/src/engine/math.py
@@ -20,26 +20,41 @@ class Math:
 
 
 class Vec2(pygame.math.Vector2):
+    _ZERO: "Vec2"
+    _ONE: "Vec2"
+    _LEFT: "Vec2"
+    _RIGHT: "Vec2"
+    _UP: "Vec2"
+    _DOWN: "Vec2"
+
     @staticmethod
     def zero() -> "Vec2":
-        return Vec2(0, 0)
+        return Vec2._ZERO.copy()
 
     @staticmethod
     def one() -> "Vec2":
-        return Vec2(1, 1)
+        return Vec2._ONE.copy()
 
     @staticmethod
     def left() -> "Vec2":
-        return Vec2(-1, 0)
+        return Vec2._LEFT.copy()
 
     @staticmethod
     def right() -> "Vec2":
-        return Vec2(1, 0)
+        return Vec2._RIGHT.copy()
 
     @staticmethod
     def up() -> "Vec2":
-        return Vec2(0, -1)
+        return Vec2._UP.copy()
 
     @staticmethod
     def down() -> "Vec2":
-        return Vec2(0, 1)
+        return Vec2._DOWN.copy()
+
+
+Vec2._ZERO = Vec2(0, 0)
+Vec2._ONE = Vec2(1, 1)
+Vec2._LEFT = Vec2(-1, 0)
+Vec2._RIGHT = Vec2(1, 0)
+Vec2._UP = Vec2(0, -1)
+Vec2._DOWN = Vec2(0, 1)
